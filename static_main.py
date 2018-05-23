057f292e57a8015a13d923382e5b6519cbba901e
Luo <luoluo1920@gmail.com>
2018-05-23 17:12:38 +0800

    v2

--- static_main.py
+++ static_main.py
@@ -7,8 +7,16 @@ resources = Resources()
 resources.init()
 app = Flask(__name__)
 with app.app_context():
-	with io.open('index.html', 'w') as output:
-		output.write(render_template("archives.html", posts=resources.posts(), title="archives"))
-	for post in resources.posts():
-		with io.open('archives/' + post.link, 'w') as output:
-			output.write(render_template("post.html", post=post, title=post.title))
+    with io.open('index.html', 'w') as output:
+        output.write(render_template("archives.html", posts=resources.posts(), title="archives"))
+
+    with io.open('about.html', 'w') as output:
+    	post = resources.get_about()
+        output.write(render_template("post.html", post=post, title="about"))
+
+    with io.open('categories.html', 'w') as output:
+        output.write(render_template("categories.html", categories=resources.categories(), title="categories"))
+
+    for post in resources.posts():
+        with io.open('archives/' + post.link, 'w') as output:
+            output.write(render_template("post.html", post=post, title=post.title))
